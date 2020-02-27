4df909d7791c250a4f8be91bb53fae0190398fac
tanaamiyumi <yumi.tanaami@rakus-partners.co.jp>
2020-02-27 15:04:41 +0900

    first commit

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Car {
+
+	private int speed;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	
+}
